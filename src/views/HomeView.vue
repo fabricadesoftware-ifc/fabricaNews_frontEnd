ce427aa82a419a386236be957b035507370fba0a
ViniciusAcordiSoethe <soethe42@gmail.com>
2023-07-28 01:01:16 -0300

    feat: Modelo base do home#5

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import NavBar from '../components/NavBar.vue'
+import Home from '../components/Home.vue'
 </script>
 
 <template>
   <main>
     <NavBar />
+    <Home />
   </main>
 </template>
