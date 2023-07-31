89ee783a18c35c1a9a3095d13ed5cf9ec5f2b4b4
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2023-07-31 14:37:34 -0300

    feat: criando o Sobre

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,11 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script setup>
+import NavBar from "../components/NavBar.vue";
+import Sobre from "../components/Sobre.vue";
+</script>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<template>
+  <main>
+    <NavBar />
+    <Sobre />
+  </main>
+</template>
