3a67877604bbc1dfc2263cd393759e688d08ef19
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2023-08-03 14:05:35 -0300

    fix: Foi adicionada a página Projetos

---
+++ src/components/Projetos.vue
@@ -0,0 +1,38 @@
+<script setup></script>
+
+<template>
+  <div class="p-4 lg:p-10 lg:w-9/12 border-b border-gray">
+    <div class="flex justify-center p-2">
+      <img class="w-24 h-auto" src="@/assets/fabrica_news_icon.svg" alt="" />
+      <h2 class="p-5 text-4xl">
+        <span class="border-l border-gray pl-3 text-5xl">f</span>News
+      </h2>
+    </div>
+  </div>
+  <div>
+    <p class="text-center">
+      A revolução na informação com notícias personalizadas em tempo real. A
+      Fabrica News é uma plataforma inovadora que utiliza algoritmos avançados
+      para oferecer notícias personalizadas e relevantes para cada usuário. Com
+      ualizações em tempo real, essa revolução na informação mantém as pessoas
+      informadas sobre os assuntos que mais lhes interessam, promovendo uma
+      experiência de leitura envolvente e completa.
+    </p>
+  </div>
+  <div class="lg:p-10 lg:w-9/12">
+    <p class="text-center">
+      Bem-vindo ao site do projeto Fábrica News! Aqui você encontrará todas as
+      notícias e informações mais recentes sobre a nossa fábrica de software, um
+      projeto emocionante desenvolvido por alunos como você. Nós nos dedicamos a
+      criar sites e sistemas inovadores, utilizando as mais recentes tecnologias
+      e metodologias de desenvolvimento. Na Fábrica News, acreditamos no poder
+      da aprendizagem prática e no desenvolvimento de habilidades no mundo real.
+      Nossa fábrica de software é um ambiente de aprendizagem colaborativa, onde
+      estudantes de diferentes cursos se reúnem para trabalhar em projetos
+      reais. Ao participar deste projeto, você terá a oportunidade de aplicar
+      seus conhecimentos teóricos em situações práticas, ganhando valiosa
+      experiência no campo do desenvolvimento de software.
+    </p>
+  </div>
+</template>
+<style scoped></style>
---
+++ src/views/ProjetosView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NavBar from "../components/NavBar.vue";
+import Projetos from "../components/Projetos.vue";
+</script>
+
+<template>
+  <header>
+    <NavBar />
+  </header>
+  <main class="w-full flex flex-col items-center">
+    <Projetos />
+  </main>
+  <footer></footer>
+</template>
