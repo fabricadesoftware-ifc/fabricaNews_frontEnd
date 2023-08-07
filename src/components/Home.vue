270e4d9dea1c6af059a8c266a022a0ca9f9f20bb
BrunnoDC <BrunnoDC>
2023-08-07 15:15:20 -0300

    criando a pagina Home

---
+++ src/components/Home.vue
@@ -0,0 +1,89 @@
+<script></script>
+<template>
+  <!-- component -->
+  <!--    
+      This a copyrighted component by https://www.chatwoot.com/
+      Only for Educational Purposes. 
+      Do not copy. 
+      Tailwind Play Link: https://play.tailwindcss.com/WE5RGyQ61x
+     -->
+
+  <div class="flex min-h-screen items-center justify-center p-10 bg-white">
+    <div
+      class="container grid max-w-screen-xl gap-8 lg:grid-cols-2 lg:grid-rows-2"
+    >
+      <div class="row-span-2 flex flex-col rounded-md border border-slate-200">
+        <div class="h-1/2 flex-1">
+          <img
+            src="/src/assets/images/Group 14.png"
+            class="w-full object-cover object-right-top"
+            alt="omnichannel"
+          />
+        </div>
+        <div class="p-10">
+          <h3 class="text-xl font-medium text-gray-700">
+            Omnichannel support center
+          </h3>
+          <p class="mt-2 text-slate-500">
+            Chatwoot connects with popular customer communication channels like
+            Email, Website live-chat, Facebook, Twitter, WhatsApp, Instagram,
+            Line, etc., and helps you deliver a consistent customer experience
+            across channels.
+          </p>
+          <a href="" class="mt-2 inline-flex text-sky-500">Read More →</a>
+        </div>
+      </div>
+
+      <div class="border border-slate-200">
+        <div
+          class="row-span-2 flex flex-col rounded-md border border-slate-200"
+        >
+          <div class="h-1/2 flex-1">
+            <img
+              src="/src/assets/images/Group 13.png"
+              class="w-full object-cover object-right-top"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="flex-1 p-10">
+          <h3 class="text-xl font-medium text-gray-700">
+            A live-chat that fits your brand
+          </h3>
+          <p class="mt-2 text-slate-500">
+            Connect with your website visitors, communicate with them in
+            realtime and give them quality support with a live-chat widget that
+            fits your brand.
+          </p>
+          <a href="" class="mt-2 inline-flex text-sky-500">Read More →</a>
+        </div>
+      </div>
+
+      <div class="border border-slate-200">
+        <div
+          class="row-span-2 flex flex-col rounded-md border border-slate-200"
+        >
+          <div class="h-1/2 flex-1">
+            <img
+              src="/src/assets/images/Group 15.png"
+              class="w-full object-cover object-right-top"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="flex-1 p-10">
+          <h3 class="text-xl font-medium text-gray-700">
+            A live-chat that fits your brand
+          </h3>
+          <p class="mt-2 text-slate-500">
+            Connect with your website visitors, communicate with them in
+            realtime and give them quality support with a live-chat widget that
+            fits your brand.
+          </p>
+          <a href="" class="mt-2 inline-flex text-sky-500">Read More →</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style></style>
