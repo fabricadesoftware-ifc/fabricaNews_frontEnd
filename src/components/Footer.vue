2f126c4b5845e5319d85f02dd7381861264f26f4
davi <davigabtomaz@gmail.com>
2023-08-24 13:09:47 -0300

    fix: Criação do Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,104 @@
+<script setup>
+import Hashtags from "../components/Hashtags.vue";
+import Icons from "../components/Icons.vue";
+</script>
+<template>
+  <footer class="bg-footer dark:bg-gray-900">
+    <Icons class="p-10 border-b border-gray" />
+    <div>
+      <div class="flex text-white justify-center p-2">
+        <img class="w-24 h-auto" src="@/assets/fabrica_news_icon.svg" alt="" />
+        <h2 class="p-5 text-4xl">
+          <span class="border-l border-gray pl-3 text-5xl">f</span>News
+        </h2>
+      </div>
+    </div>
+    <div class="mx-auto w-2/5 max-w-screen-xl">
+      <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
+        <Hashtags
+          title="Hashtags"
+          :items="[
+            {
+              name: 'Link 1',
+              link: 'link',
+            },
+            {
+              name: 'Link 2',
+              link: 'link',
+            },
+            {
+              name: 'Link 3',
+              link: 'link',
+            },
+            {
+              name: 'Link 4',
+              link: 'link',
+            },
+          ]"
+        />
+        <Hashtags
+          title="Hashtags"
+          :items="[
+            {
+              name: 'Link 1',
+              link: 'link',
+            },
+            {
+              name: 'Link 2',
+              link: 'link',
+            },
+            {
+              name: 'Link 3',
+              link: 'link',
+            },
+            {
+              name: 'Link 4',
+              link: 'link',
+            },
+          ]"
+        />
+        <Hashtags
+          title="Menu"
+          :items="[
+            {
+              name: 'Explorar',
+            },
+            {
+              name: 'Todas as Tags',
+            },
+            {
+              name: 'Sobre nós',
+            },
+            {
+              name: 'Dúvidas Frequentes',
+            },
+          ]"
+        />
+        <Hashtags
+          title="Informações"
+          :items="[
+            {
+              name: 'Minha Conta',
+            },
+            {
+              name: 'Participe da Fábrica',
+            },
+            {
+              name: 'Seja Colaborador ',
+            },
+            {
+              name: 'Fale Conosco',
+            },
+            {
+              name: 'Anuncie',
+            },
+          ]"
+        />
+      </div>
+    </div>
+    <div class=" p-5 flex justify-center">
+    <img class="w-24" src="@/assets/Group-1.svg"/>  
+      <img class="w-24" src="@/assets/image.png"/>  
+    </div>
+  </footer>
+</template>
---
+++ src/components/Hashtags.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        items: {
+            type: Array,
+            required: true
+        }
+    },
+    methods: {
+        getLink(item) {
+            return item.link ? item.link : item.name
+        }
+    }
+}
+
+</script>
+<template>
+  <div>
+    <h2
+      class="mb-6 text-sm font-semibold text-white uppercase dark:text-white"
+    > 
+    {{ title }}
+    </h2>
+    <ul  class="text-gray-500 dark:text-gray-400 font-medium">
+      <li v-for="item in items"  class="mb-4">
+        <a :href='getLink(item)'  class="hover:underline"> {{ item.name }} </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
